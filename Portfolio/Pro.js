2f1544e882abd792844d5ac0e1969e016b8df913
Akintimehin <152616564+Akintimehin@users.noreply.github.com>
2024-07-30 18:47:26 +0100

    Add files via upload

---
+++ Portfolio/Pro.js
@@ -0,0 +1,14 @@
+function everyMenu() {
+    var intro = document.getElementById("intro")
+    if (intro.style.display==="flex") {
+        intro.style.display="none"
+        
+    }
+    else{
+        intro.style.display==="flex"
+    }
+    
+}
+
+
+
